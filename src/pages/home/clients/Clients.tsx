ca8e8b7ccfabd541b856ac2da395ea7eb42c804c
Yahckman <adeniranajibade2014@gmail.com>
2023-11-14 11:39:24 +0100

    finishing up the card

--- src/pages/home/clients/Clients.tsx
+++ src/pages/home/clients/Clients.tsx
@@ -1,4 +1,3 @@
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
@@ -7,6 +6,11 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import ClientsCard from "./ClientsCard";
+
+// import "swiper/components/navigation/navigation.css";
+
+// Import Swiper core and required modules
+
 
 function Clients() {
   const customerInfo = [
@@ -57,7 +61,7 @@ function Clients() {
         breakpoints={{
           320: {
             width: 320,
-            slidesPerView: 1.05,
+            slidesPerView: 1.03,
             spaceBetween: 10,
           },
           640: {
@@ -75,6 +79,7 @@ function Clients() {
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
         slidesPerView={3}
+        // navigation
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
@@ -82,13 +87,20 @@ function Clients() {
         onSwiper={(swiper) => console.log(swiper)}
         speed={3000}
       >
-
-
         {customerInfo.map((info: any, i: number) => (
           <SwiperSlide>
-            <ClientsCard key={i} name={info.name} date={info.date} remark={info.remark} comment={info.comment} />
+            <ClientsCard
+              key={i}
+              name={info.name}
+              date={info.date}
+              remark={info.remark}
+              comment={info.comment}
+            />
           </SwiperSlide>
         ))}
+
+
+      
       </Swiper>
     </div>
   );
--- src/pages/home/clients/ClientsCard.tsx
+++ src/pages/home/clients/ClientsCard.tsx
@@ -1,38 +1,33 @@
+import styles from "./client.module.scss";
 
-import styles from "./client.module.scss"
-
-interface ClientData{
-    name?:string
-    date?: string
-    remark?: string
-    comment?: string
-    key:number
+interface ClientData {
+  name?: string;
+  date?: string;
+  remark?: string;
+  comment?: string;
+  key: number;
 }
 
-function ClientsCard({name, date, remark, comment, key}:ClientData) {
-   
+function ClientsCard({ name, date, remark, comment, key }: ClientData) {
   return (
-    <div><div
-    className={`bg-red-400 h-[70vh] relative px-7 py-7 ${styles.card}`}
-  key={key}
-  >
-    <div className=" h-[50%] w-full  px-10 bg-[#31124bc1] justify absolute bottom-0 left-0 ">
-      <div className="bg-[#FFFFFF] py-2 px-4 pr-10 leading-7 text-[#2d2c2c] w-fit -mt-8 mb-8 ">
-        <p className=" font-bold uppercase font-sm">{name}</p>
-        <p className="text-xs">{date}</p>
-      </div>
-      <div className="text-white ">
-      <p className="text-2xl mb-4 font-semibold">
-        " {remark} "
-      </p>
-      <p className="text-[15px] leading-7">
-    {comment}
-      </p>
-
+    <div>
+      <div
+        className={`bg-red-400 h-[70vh] relative px-7 py-7 ${styles.card}`}
+        key={key}
+      >
+        <div className=" h-[50%] w-full  px-10 bg-[#31124bc1] justify absolute bottom-0 left-0 ">
+          <div className="bg-[#FFFFFF] py-2 px-4 pr-10 leading-7 text-[#2d2c2c] w-fit -mt-8 mb-8 ">
+            <p className=" font-bold uppercase font-sm">{name}</p>
+            <p className="text-xs">{date}</p>
+          </div>
+          <div className="text-white ">
+            <p className="text-2xl mb-4 font-semibold">" {remark} "</p>
+            <p className="text-[15px] leading-7">{comment}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div></div>
-  )
+  );
 }
 
-export default ClientsCard
+export default ClientsCard;
