b457e76a17afab7a2d1255a3d9b38679487f5751
Yahckman <adeniranajibade2014@gmail.com>
2023-11-16 09:37:05 +0100

    footer resonsive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -27,32 +27,32 @@ function Footer() {
         {
           name: "No 16 Odo Oba Moniya Ibadan",
           path: "#",
-          icon: <ImLocation size={25} />,
+          icon: <ImLocation size={20} />,
         },
         {
           name: "+2348146283237",
           path: "#",
-          icon: <MdPhoneInTalk size={25} />,
+          icon: <MdPhoneInTalk size={20} />,
         },
         {
           name: "alItmaam21243@yahoo.com",
           path: "#",
-          icon: <HiOutlineMail size={25} />,
+          icon: <HiOutlineMail size={20} />,
         },
       ],
     },
     {
       title: "Connect with Us",
       links: [
-        { name: "Facebook", path: "#", icon: <FaFacebookF size={25} /> },
-        { name: "Twitter", path: "#", icon: <FaTwitter size={25} /> },
-        { name: "LinkdIn", path: "#", icon: <FaInstagram size={25} /> },
-        { name: "LinkdIn", path: "#", icon: <FaLinkedinIn size={25} /> },
+        { name: "Facebook", path: "#", icon: <FaFacebookF size={20} /> },
+        { name: "Twitter", path: "#", icon: <FaTwitter size={20} /> },
+        { name: "LinkdIn", path: "#", icon: <FaInstagram size={20} /> },
+        { name: "LinkdIn", path: "#", icon: <FaLinkedinIn size={20} /> },
       ],
     },
   ];
   return (
-    <div className="bg-[#31124b] min-h-[85vh] grid grid-cols-4 text-white font-sans px-16 gap-10 py-24">
+    <div className="bg-[#31124b] min-h-[85vh] md:grid md:grid-cols-2  lg:grid-cols-4 text-white font-sans px-5 md:px-16 gap-10 py-24">
       <div>
         <p className="font-extrabold  text-2xl md:text-4xl uppercase mb-7">
           Al-Itmaam
@@ -66,14 +66,17 @@ function Footer() {
 
       {footerData.map((item: any, i: number) => (
         <div key={i}>
-          <p className="  font-bold mb-7 text-lg pt-3 ">{item.title}</p>
+          <p className="  font-bold uppercase mb-7 md:text-lg text-[15px] mt-7 pt-3 ">
+            {item.title}
+          </p>
 
           {item.links.map((data: any, i: any) => (
-            <div className="leading-8 mb-3 font-semibold " key={i}>
+            <div className="leading-8 mb-3  " key={i}>
               {" "}
               <Link to={data.path} className="flex">
                 {" "}
-                <span className="mr-5"> {data.icon} </span> {data.name}
+                {data.icon && <span className="mr-5"> {data.icon} </span>}
+                {data.name}
               </Link>{" "}
             </div>
           ))}
