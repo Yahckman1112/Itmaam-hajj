5b0d87c9a4bfa8dd071e9fefb0d98383b40ded8d
Yahckman <adeniranajibade2014@gmail.com>
2023-11-17 15:54:09 +0100

    ading progressBar to card and adding its logic

--- src/pages/packages/Packages.tsx
+++ src/pages/packages/Packages.tsx
@@ -4,8 +4,85 @@ import packageBg from '../../assets/packagesBg.jpg'
 import img1 from '../../assets/3.jpg'
 import MakkahHotel from '../../assets/hotel-1.png'
 import MadinahHotel from '../../assets/hotel.png'
-import CustomButton from './../../components/customButton/CustomButton';
+// @ts-ignore
+import ProgressBar from 'react-progressbar'
+
+
 function Packages() {
+    const packageData = [
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+        {
+            img:img1,
+            name: 'Hajj Package',
+            time: 20,
+            currentPrice: 15000,
+            lastPrice: 18000,
+            registeredClient:55,
+            totalSpace:160,
+
+        },
+    ]
+    // const progress = (ragisteredClient/totalSpace)
+    const progress = (123/150)*100
   return (
     <div>
         <div>
@@ -14,23 +91,25 @@ function Packages() {
         <section className='p-20 min-h-screen'>
             <p className='text-center text-2xl font-semibold'  > Our Packages</p>
             <div className='grid grid-cols-3 gap-7'>
-                <div className='rounded-lg border border-solid  '>
+            {packageData.map((item:any,i:any)=>(
+
+                <div className='rounded-lg border border-solid  ' key={i}>
           
                     <div>
 
-                    <img src={img1} alt="" className='  object-cover' />
+                    <img src={item.img} alt="" className='  object-cover' />
                     </div>
                     <div className='p-7'>
 
                     
-                    <p className='text-xl font-semibold'>Umrah Packages</p>
+                    <p className='text-xl font-semibold'>{item.name}</p>
                     <div className='grid grid-cols-2 my-7 ' style={{gridTemplateColumns:'20% 80%'}}>
                         <div>
                             <img src={MakkahHotel} width={'50px'} alt="" className='flex m-aut0' />
                         </div>
                         <div >
                             <p className='text-[15px] font-semibold mb-1 '>MAKKAH : Hotel Name</p>
-                            <p className='text-[15px] font-bold text-[#007bff] '>20 MIN To Masjidil Haram</p>
+                            <p className='text-[15px] font-bold text-[#007bff] '>{item.time} MIN To Masjidil Haram</p>
                         </div>
 
                     </div>
@@ -40,20 +119,28 @@ function Packages() {
                         </div>
                         <div >
                             <p className='text-[15px] font-semibold mb-1 '>MAKKAH : Hotel Name</p>
-                            <p className='text-[15px] font-bold text-[#007bff] '>20 MIN To Nabawi Mosque</p>
+                            <p className='text-[15px] font-bold text-[#007bff] '>{item.time} MIN To Nabawi Mosque</p>
                         </div>
 
                     </div>
 
 
-                    <p className='text-[#007bff] text-2xl font-semibold my-12 mb-7' > <span className='text-[#929191] line-through text-lg font-normal'>16.0000 USD/P</span> 15.0000 USD/P </p>
+                    <p className='text-[#007bff] text-2xl font-semibold my-12 mb-7' > <span className='text-[#929191] line-through text-lg font-normal'>{item.lastPrice} USD/P</span> {item.currentPrice} USD/P </p>
 
                         <button className='text-base text-black font-bold uppercase px-12 py-3  bg-[#e5efffbc] hover:bg-[#ffc107] transition-transform duration-500 transform  rounded-full'>Apply Now</button>
                         
                     </div>
+                        <div className='my-9 border border-solid border-t-1 border-b-0 border-r-0 border-l-0 pt-7 p-3'>
+                            <div className='w-full border border-solid rounded-lg'>
+
+                            < ProgressBar completed={(item.registeredClient/item.totalSpace)*100} />
+
+                            </div>
+                            <p className='mt-3 text-xs font-semibold'>Booked : {item.registeredClient}/{item.totalSpace}</p>
+                        </div>
                 </div>
-                <div>2</div>
-                <div>3</div>
+            ))}
+              
             </div>
         </section>
         </div>
