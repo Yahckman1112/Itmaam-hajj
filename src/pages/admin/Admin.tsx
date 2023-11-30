e9991c1580439480ff8b2dce70ecc6c86bad11de
Yahckman <adeniranajibade2014@gmail.com>
2023-11-30 10:40:05 +0100

    validate form

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,7 +1,29 @@
 import { Label, LoginContainer } from "./admin.style";
 // import banner from "../../assets/loginbg.png";
 import { LoginInput } from "./admin.style";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 function Admin() {
+  const loginValidate = () => {
+    return Yup.object({
+      email: Yup.string().required("enter your valid email").email(),
+      password: Yup.string().required("Let us Know ur name"),
+    });
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginValidate(),
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div
       className="h-screen p-5  bg-[#F1F6F9] m-auto flex justify-center items-center"
@@ -9,22 +31,47 @@ function Admin() {
     >
       <LoginContainer className="px-10 py-12 bg-white  border-x-0 border-y-0  max-w-[450px] font-[open-sans] rounded-lg border border-solid border-t-[5px] border-[#1A8F4A]">
         <p className="text-[#1A8F4A] mb-7 font-bold text-4xl ">Login</p>
-        <div className="mb-12">
-          <Label>Email</Label>
-          <LoginInput placeholder="email" />
-        </div>
+        <form action="" onSubmit={formik.handleSubmit}>
+          <div className="mb-12">
+            <Label>Email</Label>
+            <LoginInput
+              placeholder="email"
+              type="text"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+                       {formik.touched.email && formik.errors.email && (
+            <p className={'text-xs mt-2 text-red-500'}>{formik.errors.email}</p>
+          )}
+          </div>
 
-        <div className="mb-12 ">
-          <Label>Password</Label>
-          <LoginInput placeholder="Password" />
-        </div>
-        <p className="text-[#1A8F4A] text-right cursor-pointer font-semibold text-base">
-          Forgot Password?{" "}
-        </p>
+          <div className="mb-12 ">
+            <Label>Password</Label>
+            <LoginInput
+              placeholder="Password"
+              type="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+                       {formik.touched.password && formik.errors.password && (
+            <p className={'text-xs mt-2 text-red-500'}>{formik.errors.password}</p>
+          )}
+          </div>
+          <p className="text-[#1A8F4A] text-right cursor-pointer font-semibold text-base">
+            Forgot Password?{" "}
+          </p>
 
-        <button className="w-full mt-5 py-3 bg-[#1A8F4A] shadow-sm rounded-sm text-[#FCFCFC] font-semibold text-base ">
-          Login
-        </button>
+          <button
+            type="submit"
+            className="w-full mt-5 py-3 bg-[#1A8F4A] shadow-sm rounded-sm text-[#FCFCFC] font-semibold text-base "
+          >
+            Login
+          </button>
+        </form>
       </LoginContainer>
     </div>
   );
--- src/pages/register/register.style.ts
+++ src/pages/register/register.style.ts
@@ -5,10 +5,3 @@ export const Input = styled.input.attrs({
     ' border-none outline-none w-full pl-3 py-3 md:py-4  text-sm font-normal  rounded-lg p-0',
 })``
 
-// color: var(--primary-color-primary-deep-accent, #1A8F4A);
-// font-family: Lato;
-// font-size: 20px;
-// font-style: normal;
-// font-weight: 600;
-// line-height: normal;
-// text-decoration-line: underline;
