6d0bfca12c64432852b8881ca58c588e100250ea
Yahckman <adeniranajibade2014@gmail.com>
2023-12-02 12:28:22 +0100

    last touch on table

--- src/components/appTable/AppTable.tsx
+++ src/components/appTable/AppTable.tsx
@@ -2,31 +2,31 @@ import { TableData, TableRow } from "./appTable.style";
 
 function AppTable() {
   return (
-    <div className=" max-w-[100%] overflow-x-scroll mb-14">
-      <table className="w-[100%] border-collapse text-sm md:text-base ">
+    <div className=" max-w-[100%] overflow-x-scroll mb-14 text-sm">
+      <table className="w-[100%] border-collapse  ">
         <thead>
             <tr>
-                <th className="border  rounded-tl-md bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0\ border-b-2 border-[rgba(167, 207, 184, 0.4)]">S/N</th>
-                <th  className="border   bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0\ border-b-2 border-[rgba(167, 207, 184, 0.4)]">Fullname</th>
-                <th  className="border   bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0\ border-b-2 border-[rgba(167, 207, 184, 0.4)]"> Email</th>
-                <th  className="border rounded-tr-md  bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0\ border-b-2 border-[rgba(167, 207, 184, 0.4)]">Role</th>
+                <th className="border text-left  rounded-tl-md bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0 border-b-2 border-[rgba(167, 207, 184, 0.4)]">S/N</th>
+                <th  className="border text-left   bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0 border-b-2 border-[rgba(167, 207, 184, 0.4)]">Fullname</th>
+                <th  className="border text-left   bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0 border-b-2 border-[rgba(167, 207, 184, 0.4)]"> Email</th>
+                <th  className="border text-left  rounded-tr-md  bg-[#1A8F4A] text-[#fff] p-4 border-solid  border-x-0 border-y-0 border-b-2 border-[rgba(167, 207, 184, 0.4)]">Role</th>
             </tr>
         </thead>
 
-        <tbody>
-            <TableRow className="text-center">
+        <tbody >
+            <TableRow className="">
                 <TableData >1</TableData>
                 <TableData >name of names</TableData>
                 <TableData >name of names</TableData>
                 <TableData >name of names</TableData>
  </TableRow>
-            <TableRow className="text-center p-4">
+            <TableRow className=" p-4">
                 <TableData >2</TableData>
                 <TableData >Adewale Tobiloba Daniel</TableData>
                 <TableData >testgmail@gmail.com</TableData>
                 <TableData >Treasurer</TableData>
  </TableRow>
-            <TableRow className="text-center">
+            <TableRow className="">
                 <TableData >3</TableData>
                 <TableData >name of names</TableData>
                 <TableData >name of names</TableData>
--- src/components/appTable/appTable.style.ts
+++ src/components/appTable/appTable.style.ts
@@ -18,7 +18,7 @@ export const TableData = styled.td.attrs({
     className:'text-sm font-semibold '
 })`
 border-bottom: 1px solid rgba(167, 207, 184, 0.4) !important;
-  padding: 8px !important;
+  padding: 18px !important;
 &:nth-child(){
 
 }
